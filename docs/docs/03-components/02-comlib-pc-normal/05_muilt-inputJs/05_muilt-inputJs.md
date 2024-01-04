31897e1d02abd7a1a044f366a32e2606c2465cd7
zhaoxing03 <zhaoxing03@kuaishou.com>
2024-01-04 18:22:04 +0800

    beta

--- docs/docs/03-components/02-comlib-pc-normal/05_muilt-inputJs/05_muilt-inputJs.md
+++ docs/docs/03-components/02-comlib-pc-normal/05_muilt-inputJs/05_muilt-inputJs.md
@@ -7,18 +7,83 @@ tags:
 ![Alt text](img/image.png)
 
 ## 何时使用
-当组件间逻辑通信没有适合的组件时，可以通过「JS计算」组件来实现。
+
+编写自定义 Javascript 代码。支持 ES6 和 Typescript，在页面发布时会被编译为 ES5。
+
+:::tip
+如果你之前并没有 Javascript 开发经验也没有关系，你可以通过 [MDN](https://developer.mozilla.org/zh-CN/docs/Learn/JavaScript/First_steps) 来快速学习，这是一门非常容易上手的语言。
+:::
 
 
-## 案例一：多个输入同时到达后
+## 案例一：多个输入
 
+### 案例介绍
+通常我们会遇到一个交互会触发执行多个并行任务，其中不乏异步的任务。我们希望在所有并行任务执行完成之后再继续进行后续任务。
+
+### 搭建案例链接
+demo：[多个输入](https://my.mybricks.world/mybricks-app-pcspa/index.html?id=517585613344837)
+
+![](img/muilt-inputs.gif)
+
+### 关键步骤
+
+通过「添加输入项」可以在节点左侧添加任意的输入项。
+
+![Alt text](img/image-1.png)
+
+通过聚焦节点左侧的输入项，可以「删除」输入项。
+
+![Alt text](img/image-2.png)
+
+在 Javascript 代码中，inputs 入参为数组，对应左侧的输入项的值，从上到下依次为数组的第0项，第1项...
+
+### 执行时机
+
+:::tip
+特别注意
+:::
+
+**每当** 左侧的所有输入项都到达后，会触发一次自定义函数的执行。inputs 的值为当前最新的所有输入项的最新值。当触发后，会清空所有的输入项，进入下一个等待周期。[点击查看 demo](https://my.mybricks.world/mybricks-app-pcspa/index.html?id=518241144234053)
 
 
 ## 案例二：多个输出
 
+### 案例介绍
+当一个交互需要给多个任务分发不同的入参时。
+
+### 搭建案例链接
+demo：[多个输出](https://my.mybricks.world/mybricks-app-pcspa/index.html?id=517585657278533)
+
+![](img/muilt-outputs.gif)
+
+### 关键步骤
+
+通过「添加输出项」可以在节点左侧添加任意的输出项。通过聚焦节点右侧的输出项，可以「删除」输出项。
+
+在 Javascript 代码中，outputs 入参为数组，对应左侧的输出项的函数，从上到下依次为数组的第0项，第1项...
+
+每执行一次函数，都会触发一次对应的输出项。
+
+![Alt text](img/image-3.png)
 
 
+## 案例三：为输出项配置数据类型
 
-## 案例三：为输出配置 schema
+### 案例介绍
+数据类型是 MyBricks 引擎中非常重要的特性，基于类型可以更快捷的进行数据映射。
 
+### 搭建案例链接
 
+demo：[为输出项配置数据类型](https://my.mybricks.world/mybricks-app-pcspa/index.html?id=517585739698245)
+
+![](img/set-schema.gif)
+
+### 关键步骤
+
+聚焦某输出项，点击编辑即可进行修改。
+
+![Alt text](img/image-4.png)
+
+在编辑类型面板中，左侧的文字可以点击进行可视化编辑，右侧的 `</>` 按钮可以点击后切换到代码模式进行编辑。类型的规范使用 [JSON Schema 规范](https://json-schema.org/)
+
+![Alt text](img/image-5.png)
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,54 +1,53 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'MyBricks 文档中心',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "MyBricks 文档中心",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://docs.mybricks.world',
+  url: "https://docs.mybricks.world",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'mybricks', // Usually your GitHub org/user name.
-  projectName: 'MyBricks', // Usually your repo name.
+  organizationName: "mybricks", // Usually your GitHub org/user name.
+  projectName: "MyBricks", // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans'],
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/mybricks/docs/tree/main/',
-          routeBasePath: '/',
+          editUrl: "https://github.com/mybricks/docs/tree/main/",
+          routeBasePath: "/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/mybricks/docs/tree/main/',
+          editUrl: "https://github.com/mybricks/docs/tree/main/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -56,19 +55,19 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'MyBricks 文档中心',
+      title: "MyBricks 文档中心",
       logo: {
-        alt: 'MyBricks低代码',
-        src: 'img/logo.png',
+        alt: "MyBricks低代码",
+        src: "img/logo.png",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: '文档',
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "文档",
         },
         // {
         //   type: 'docSidebar',
@@ -77,12 +76,12 @@ const config: Config = {
         //   label: '组件库',
         // },
         {
-          type: 'docSidebar',
-          sidebarId: 'trainingSidebar',
-          position: 'left',
-          label: '课程',
+          type: "docSidebar",
+          sidebarId: "trainingSidebar",
+          position: "left",
+          label: "课程",
         },
-        {to: '/blog', label: '博客', position: 'left'},
+        { to: "/blog", label: "博客", position: "left" },
         // {
         //   href: 'https://github.com/mybricks',
         //   label: 'GitHub',
@@ -92,41 +91,41 @@ const config: Config = {
     },
     footer: {
       logo: {
-        alt: 'MyBricks低代码',
-        src: 'img/logo.png',
+        alt: "MyBricks低代码",
+        src: "img/logo.png",
         width: 50,
         height: 50,
       },
       copyright: `Copyright © ${new Date().getFullYear()} MyBricks.`,
       links: [
         {
-          title: '产品',
+          title: "产品",
           items: [
             {
-              label: 'MyBricks-SPA 低代码引擎',
-              href: 'https://mybricks.world',
+              label: "MyBricks-SPA 低代码引擎",
+              href: "https://mybricks.world",
             },
             {
-              label: 'VsCode插件',
-              href: 'https://marketplace.visualstudio.com/items?itemName=Mybricks.Mybricks&ssr=false#overview'
-            }
-          ],
-        },
-        {
-          title: '文档',
-          items: [
-            {
-              label: '快速开始',
-              to: 'docs/category/quick-start',
+              label: "VsCode插件",
+              href: "https://marketplace.visualstudio.com/items?itemName=Mybricks.Mybricks&ssr=false#overview",
             },
           ],
         },
         {
-          title: '链接',
+          title: "文档",
           items: [
             {
-              label: 'Github',
-              href: 'https://github.com/mybricks',
+              label: "快速开始",
+              to: "docs/category/quick-start",
+            },
+          ],
+        },
+        {
+          title: "链接",
+          items: [
+            {
+              label: "Github",
+              href: "https://github.com/mybricks",
             },
             // {
             //   html: `
@@ -148,13 +147,13 @@ const config: Config = {
     },
   } satisfies Preset.ThemeConfig,
   markdown: {
-    format: 'detect' // 识别md和mdx后缀自动编译，md会走普通md编译，只有mdx才会走mdx编译
+    format: "detect", // 识别md和mdx后缀自动编译，md会走普通md编译，只有mdx才会走mdx编译
   },
   themes: [
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-      ({
+      {
         // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
@@ -162,7 +161,7 @@ const config: Config = {
         // ```
         language: ["en", "zh"],
         // ```
-      }),
+      },
     ],
   ],
 };
